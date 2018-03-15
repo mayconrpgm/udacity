04c09fa7f0a9d6f0cfe3414fca951e7dbca64ee1
Maycon Queiros <maycon.queiros@nuance.com>
2018-03-15 09:45:42 -0300

    class 7 quiz 9

---
+++ Hadoop/Aula 7/quiz9reducer.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+import sys
+
+sales_total = 0
+sales_count = 0
+old_key = None
+
+# Loop around the data
+# It will be in the format key\tval
+# Where key is the store name, val is the sale amount
+#
+# All the sales for a particular store will be presented,
+# then the key will change and we'll be dealing with the next store
+
+for line in sys.stdin:
+    data_mapped = line.strip().split("\t")
+    if len(data_mapped) != 2:
+        # Something has gone wrong. Skip this line.
+        continue
+
+    this_key, this_sale = data_mapped
+
+    if old_key and old_key != this_key:
+        print old_key, "\t", (sales_total / sales_count)
+        old_key = this_key;
+        sales_total = 0
+        sales_count = 0
+
+    old_key = this_key
+    sales_total += float(this_sale)
+    sales_count += 1
+
+if old_key != None:
+    print old_key, "\t", (sales_total / sales_count)
+
